1eb6d154c574502da7a2667beb1cb67cf9043b49
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-15 13:14:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -47,9 +47,9 @@ if response.status_code != 200:
 **HOW THIS WORKS?**
 Once you've provided the necessary information, our advanced algorithm will analyze your data and generate a list of companies that align with  your preferences. You'll receive valuable insights about each company's  work-life balance, career growth opportunities, and collaborative  environment, enabling you to make an informed decision about your future workplace as a data scientist.
 
-## :trophy: :trophy: :trophy: RESULTS :trophy: :trophy: :trophy:
+## :trophy: RESULTS :trophy: 
 
-Please find the overview of your ideal company as a data scientist as follows
+Please find the overview of your ideal company as a data scientist as follows:
 '''
 st.write('Your ranking is:')
 st.write(response.json())
