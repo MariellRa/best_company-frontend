dd037bd824cbcab577f396ff153f37ee57289370
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-15 12:43:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ import math
 '''
 # Welcome to app Find DS Company!
 
-Welcome to our app, where we assist you in finding your ideal company as a data scientist. To begin the process, we kindly ask you to input three keywords that reflect your preferences and assign each a value from 1 to 10. These keywords will help our algorithm identify the best matching companies for you based on your specified criteria.
+Welcome to our app, where we assist you in finding your ideal company as a data scientist. 
+
+To begin the process, we kindly ask you to input three keywords that reflect your preferences and assign each a value from 1 to 10. These keywords will help our algorithm identify the best matching companies for you based on your specified criteria.
 
 The higher the value, the more important the keyword is to you.
 '''
