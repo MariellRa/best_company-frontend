ba0b964195d62d10b789a2cef9224e5da332adae
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-16 17:52:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -226,14 +226,17 @@ if company == 'Meta':
             Y = f'In addition to this...{company_name} is best know for having a great management...'
     st.write(X + Y)
 
-    meta = 'smart people, interest problems, work environment, learn lot, worklife balance, pay benefit, move fast, place work, people work, career growth, take care, work life, open culture, company culture, nice people, people nice, fast pace, people benefit, really smart, talented coworkers, pay smart, challenge work, smart colleagues, nice smart, challenge problems, culture people, life balance, free food, wellness benefit, benefit nice, love work, work smart, benefit pay, every day, benefit smart, learn new'
+    meta_cloud_data = 'smart people, interest problems, work environment, learn lot, worklife balance, pay benefit, move fast, place work, people work, career growth, take care, work life, open culture, company culture, nice people, people nice, fast pace, people benefit, really smart, talented coworkers, pay smart, challenge work, smart colleagues, nice smart, challenge problems, culture people, life balance, free food, wellness benefit, benefit nice, love work, work smart, benefit pay, every day, benefit smart, learn new'
 
-    wordcloud_meta = WordCloud(max_font_size=60, background_color='white', colormap='Paired').generate(meta)
-
-    plt.imshow(wordcloud_meta, interpolation='bilinear')
-    plt.axis("off")
-    plt.show()
-    st.pyplot(plt)
+    wordcloud_meta = WordCloud(width=800, height=400, background_color='white', contour_width=3, contour_color='steelblue', random_state=42).generate(meta_cloud_data)
+    
+    fig, ax = plt.subplots()
+    ax.imshow(wordcloud_meta, interpolation='bilinear')
+    ax.axis("off")
+    fig.set_facecolor('white')
+    fig.patch.set_alpha(0.7)
+    
+    st.pyplot(fig)
 
 if company == 'Microsoft':
     st.write('Microsoft Corporation is an American multinational technology corporation headquartered in Redmond, Washington. Microsofts best-known software products are the Windows line of operating systems, the Microsoft Office suite, and the Internet Explorer and Edge web browsers.')
@@ -281,14 +284,17 @@ if company == 'Microsoft':
             Y = f'In addition to this...{company_name} is best know for having a great management...'
     st.write(X + Y)
 
-    microsoft = 'work life, life balance, worklife balance, work culture, work environment, smart people, company culture, people work, place work, talented people, diversity inclusion, company work, interest work, nice work, flexible work, growth mindset, opportunity work, work project, culture work, team members, benefit work, interest problems, company lot, work atmosphere, balance work, work hours, people around, work talented, culture people, lot opportunities, balance lot, stable company, balance smart'
+    microsoft_cloud_data = 'work life, life balance, worklife balance, work culture, work environment, smart people, company culture, people work, place work, talented people, diversity inclusion, company work, interest work, nice work, flexible work, growth mindset, opportunity work, work project, culture work, team members, benefit work, interest problems, company lot, work atmosphere, balance work, work hours, people around, work talented, culture people, lot opportunities, balance lot, stable company, balance smart'
 
-    wordcloud_microsoft = WordCloud(max_font_size=60, background_color='white', colormap='Paired').generate(microsoft)
-
-    plt.imshow(wordcloud_microsoft, interpolation='bilinear')
-    plt.axis("off")
-    plt.show()
-    st.pyplot(plt)
+    wordcloud_microsoft = WordCloud(width=800, height=400, background_color='white', contour_width=3, contour_color='steelblue', random_state=42).generate(microsoft_cloud_data)
+    
+    fig, ax = plt.subplots()
+    ax.imshow(wordcloud_microsoft, interpolation='bilinear')
+    ax.axis("off")
+    fig.set_facecolor('white')
+    fig.patch.set_alpha(0.7)
+    
+    st.pyplot(fig)
 
 if company == 'Amazon':
     st.write('Amazon.com, Inc. is an American multinational technology company focusing on e-commerce, cloud computing, online advertising, digital streaming, and artificial intelligence.')
@@ -336,14 +342,17 @@ if company == 'Amazon':
             Y = f'In addition to this...{company_name} is best know for having a great management...'
     st.write(X + Y)
 
-    amazon = 'smart people, place work, lot learn, work life, life balance, interest problems, people work, work environment, work culture, learn lot, worklife balance, work amazon, company culture, best place, career growth, new things, interest work, learn new, opportunities learn, company work, work experience, challenge problems, compensation package, flexible work, get work, leadership principles, learn curve, many opportunities, career development, work home, work hours, growth opportunities, talented people, challenge work, opportunity learn, nice work, high salary, learn work, interest project, nice people, grow fast, place learn, love work, opportunity work, work high, people around, new ideas, work potential, try new, work project, machine learn, things learn, team work'
+    amazon_cloud_data = 'smart people, place work, lot learn, work life, life balance, interest problems, people work, work environment, work culture, learn lot, worklife balance, work amazon, company culture, best place, career growth, new things, interest work, learn new, opportunities learn, company work, work experience, challenge problems, compensation package, flexible work, get work, leadership principles, learn curve, many opportunities, career development, work home, work hours, growth opportunities, talented people, challenge work, opportunity learn, nice work, high salary, learn work, interest project, nice people, grow fast, place learn, love work, opportunity work, work high, people around, new ideas, work potential, try new, work project, machine learn, things learn, team work'
 
-    wordcloud_amazon = WordCloud(max_font_size=60, background_color='white', colormap='Paired').generate(amazon)
-
-    plt.imshow(wordcloud_amazon, interpolation='bilinear')
-    plt.axis("off")
-    plt.show()
-    st.pyplot(plt)
+    wordcloud_amazon = WordCloud(width=800, height=400, background_color='white', contour_width=3, contour_color='steelblue', random_state=42).generate(amazon_cloud_data)
+    
+    fig, ax = plt.subplots()
+    ax.imshow(wordcloud_amazon, interpolation='bilinear')
+    ax.axis("off")
+    fig.set_facecolor('white')
+    fig.patch.set_alpha(0.7)
+    
+    st.pyplot(fig)
 
 if company == 'Google':
     st.write('Google LLC is an American multinational technology company focusing on artificial intelligence, online advertising, search engine technology, cloud computing, computer software, quantum computing, e-commerce, and consumer electronics.')
@@ -391,14 +400,17 @@ if company == 'Google':
             Y = f'In addition to this...{company_name} is best know for having a great management...'
     st.write(X + Y)
 
-    google = 'smart people, work life, life balance, best company, free food, work environment, place work, worklife balance, work culture, work google, nice people, learn lot, people work, company work, every day, benefit work, nice work'
+    google_cloud_data = 'smart people, work life, life balance, best company, free food, work environment, place work, worklife balance, work culture, work google, nice people, learn lot, people work, company work, every day, benefit work, nice work'
 
-    wordcloud_google = WordCloud(max_font_size=60, background_color='white', colormap='Paired').generate(google)
-
-    plt.imshow(wordcloud_amazon, interpolation='bilinear')
-    plt.axis("off")
-    plt.show()
-    st.pyplot(plt)
+    wordcloud_google = WordCloud(width=800, height=400, background_color='white', contour_width=3, contour_color='steelblue', random_state=42).generate(google_cloud_data)
+    
+    fig, ax = plt.subplots()
+    ax.imshow(wordcloud_google, interpolation='bilinear')
+    ax.axis("off")
+    fig.set_facecolor('white')
+    fig.patch.set_alpha(0.7)
+    
+    st.pyplot(fig)
 
 '''
 
