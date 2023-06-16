c40093126ab2038c1b0eeafee2f5359ee067e031
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-16 12:43:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -108,8 +108,31 @@ sorted_variable_dict = sorted(variable_dict.items(), key=lambda x:x[1])
 work_life_intro = f'{company_name} is know for good {keyword1}'
 
 if company == 'Apple':
-    st.write('Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, the world's biggest company by market capitalization, and the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world.'
-
+    st.write('Apple Inc. is an American multinational technology company headquartered in Cupertino, California. Apple is the world's largest technology company by revenue, the world's biggest company by market capitalization, and the fourth-largest personal computer vendor by unit sales and the second-largest mobile phone manufacturer in the world.')
+    for i in range(2):
+        if list(variable_dict)[i] == keyword1:
+            X = f'{company_name} is best know for having a great work-life balance...'
+        if list(variable_dict)[i] == keyword2:
+            X = f'{company_name} is best know for having a great salary...'
+        if list(variable_dict)[i] == keyword3:
+            X = f'{company_name} is best know for having great Benefits...'
+        if list(variable_dict)[i] == keyword4:
+            X = f'{company_name} is best know for having a great company size...'
+        if list(variable_dict)[i] == keyword5:
+            X = f'{company_name} is best know for having a great management...'
+    for i in range(2):
+        if list(variable_dict)[i] == keyword1:
+            Y = f'In addition to this...{company_name} is best know for having a great work-life balance...'
+        if list(variable_dict)[i] == keyword2:
+            Y = f'In addition to this...{company_name} is best know for having a great salary...'
+        if list(variable_dict)[i] == keyword3:
+            Y = f'In addition to this...{company_name} is best know for having great Benefits...'
+        if list(variable_dict)[i] == keyword4:
+            Y = f'In addition to this...{company_name} is best know for having a great company size...'
+        if list(variable_dict)[i] == keyword5:
+            Y = f'In addition to this...{company_name} is best know for having a great management...'
+    st.write(X + Y)
+    
 if company == 'Meta':
     st.write('Meta Platforms, Inc., formerly named Facebook, Inc., and TheFacebook, Inc., is an American multinational technology conglomerate based in Menlo Park, California. The company owns Facebook, Instagram, and WhatsApp, among other products and services.')
     for i in range(2):
