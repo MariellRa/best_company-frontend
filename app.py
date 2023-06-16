09da190bcf2ea4119af5bb2716a449e3957042a2
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-16 17:12:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import requests
 import pandas as pd
 import numpy as np
 import math
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
