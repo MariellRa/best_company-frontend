6e000ab97ebf30d0c6d5b0747203e8d6370bdeab
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-19 17:30:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -184,7 +184,6 @@ if company == 'Apple':
     st.pyplot(fig)
 
 st.write('How former Employees have rated Apple?')
-st.write('Results are in %') 
   
 ratings_data_apple = pd.DataFrame({
     "AppleRatings": ["5","4","3","2","1"],
