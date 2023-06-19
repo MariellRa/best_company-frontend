9b394e60a2123a4ae5f7c9b69c7b10d61b980000
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-20 00:07:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -171,17 +171,19 @@ if company == 'Apple':
             Y = f'In addition to this...{company_name} is best know for having a great management...'
     st.write(X + Y)
 
-    apple_cloud_data = 'place work, smart people, environment people, work life, company work, big company, work smart, lot perk, well manage, pay phone, team work, work environment, life balance, people work, company culture, work apple, salary benefit, work culture, worklife balance, get work, work lot, excite work, really enjoy, culture people, fast pace, benefit pay, environment work, interest project, talented people, perk benefit, growth opportunities, lot opportunities, experience work, learn lot, opportunities grow, nice place, work people, depend team, interest work, want work, work benefit, work big, nice work, love work, work products, products impact, impact world, health insurance'
-    
-    wordcloud_apple = WordCloud(width=800, height=400, background_color='white', contour_width=3, contour_color='steelblue', random_state=42).generate(apple_cloud_data)
+    with st.expander("Wordcloud"):
 
-    fig, ax = plt.subplots()
-    ax.imshow(wordcloud_apple, interpolation='bilinear')
-    ax.axis("off")
-    fig.set_facecolor('white')
-    fig.patch.set_alpha(0.7)
+        apple_cloud_data = 'place work, smart people, environment people, work life, company work, big company, work smart, lot perk, well manage, pay phone, team work, work environment, life balance, people work, company culture, work apple, salary benefit, work culture, worklife balance, get work, work lot, excite work, really enjoy, culture people, fast pace, benefit pay, environment work, interest project, talented people, perk benefit, growth opportunities, lot opportunities, experience work, learn lot, opportunities grow, nice place, work people, depend team, interest work, want work, work benefit, work big, nice work, love work, work products, products impact, impact world, health insurance'
     
-    st.pyplot(fig)
+        wordcloud_apple = WordCloud(width=800, height=400, background_color='white', contour_width=3, contour_color='steelblue', random_state=42).generate(apple_cloud_data)
+
+        fig, ax = plt.subplots()
+        ax.imshow(wordcloud_apple, interpolation='bilinear')
+        ax.axis("off")
+        fig.set_facecolor('white')
+        fig.patch.set_alpha(0.7)
+    
+        st.pyplot(fig)
 
     with st.expander("How former Employees have rated Apple?"):
         ratings_data_apple = pd.DataFrame({
