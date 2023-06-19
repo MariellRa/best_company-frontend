9e73915637674fd0cddc5737ddb85544d109e138
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-19 12:50:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import math
 import matplotlib.pyplot as plt
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from streamlit_gallery import apps, components
+from streamlit_gallery.utils.page import page_group
 
 keyword1 = 'Career Growth'
 keyword2 = 'Work Environment'
