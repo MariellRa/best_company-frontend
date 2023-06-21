f5d3d1a6fecd366d058781bb46ba2499bbc62bd9
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-21 11:30:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,13 @@ st.sidebar.markdown("**Apps**")
 st.sidebar.write("Find DS Company")
 
 '''
-# Welcome to app :blue[TechMatch]!
+# Welcome to :blue[TechMatch]!
 
-Welcome to our app, where we assist you in finding your ideal company as a data scientist. 
-
+We assist you in finding your ideal company as a data scientist. 
 '''
+with st.expander("How this works?"):
+    st.write("Once you've provided the necessary information, our advanced algorithm will analyze your data and generate a list of companies that align with  your preferences. You'll receive valuable insights about each company's  work-life balance, career growth opportunities, and collaborative  environment, enabling you to make an informed decision about your future workplace as a data scientist.")
+
 st.write("To begin the process, we kindly ask you to rate ten keywords to reflect your preferences.") 
 st.write("These keywords will help our algorithm identify the best matching companies for you based on your specified criteria.") 
 
@@ -82,10 +84,6 @@ response = requests.get(url, params=dict_input)
 
 if response.status_code != 200:
     print('Error')
-
-with st.expander("How this works?"):
-    st.write("Once you've provided the necessary information, our advanced algorithm will analyze your data and generate a list of companies that align with  your preferences. You'll receive valuable insights about each company's  work-life balance, career growth opportunities, and collaborative  environment, enabling you to make an informed decision about your future workplace as a data scientist.")
-
 '''
 
 ## :trophy: RESULTS :trophy: 
