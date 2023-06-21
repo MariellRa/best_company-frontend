8ab982738885f202375d3dd6a6bd3536052ae726
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-21 16:29:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,6 @@ else:
     ## :dart: Your closest match :dart:
     '''
     st.write(" ")
-    st.write(" ")
     
     company_n = (list(response.json()['sorted_company'])[-1]).lower()
     image = Image.open(f'./images/{company_n}.png')
