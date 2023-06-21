b2341ca55be0a44ff3c331666baa51b1f06e5ae2
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-21 13:07:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ We assist you in finding your ideal company as a data scientist.
 with st.expander("How does this work?"):
     st.write("Once you've provided the necessary information, our advanced algorithm will analyze your data and generate a list of companies that align with  your preferences. You'll receive valuable insights about each company's  work-life balance, career growth opportunities, and collaborative  environment, enabling you to make an informed decision about your future workplace as a data scientist.")
 
-st.write("To begin the process, we kindly ask you to rate ten keywords to reflect your preferences.") 
+st.write("To begin the process, rate ten keywords to reflect your preferences.") 
 st.write("Please assign each a value from 0 to 10. The higher the value, the more important the keyword is to you.")
 
 col1, col2 = st.columns(2)
