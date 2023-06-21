82080094896444e73eb9df11e3d5968c4e0b263d
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-21 15:10:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ if sum(list(response.json()['sorted_company'].values())) == 0:
 else:
     '''
     
-    ## YOUR CLOSEST MATCH:
+    ## Your closest match:
     '''
     st.write(list(response.json()['sorted_company'])[-1])
 
