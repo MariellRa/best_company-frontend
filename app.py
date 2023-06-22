873a50245f4bd67dba5c7764de77b272f84f7140
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-22 15:58:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ keyword10 = 'Role scope'
 st.sidebar.markdown("**Apps**")
 st.sidebar.write("TechMatch")
 
-'''
 st.write("<h1>Welcome to <span style='color: #32AAD9
 ;'>TechMatch</span>!</h1>", unsafe_allow_html=True)
 
