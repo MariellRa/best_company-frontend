0dfc17f0ad4462bbe6c727f1e9ef40e67c17ab44
MariellRa <131381118+MariellRa@users.noreply.github.com>
2023-06-22 08:51:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ if response.status_code != 200:
 
 if sum(list(response.json()['sorted_company'].values())) == 0:
     st.write(" ")
-    st.write(":exclamation: No results yet!")
+    st.write(":exclamation:No results yet!:exclamation:")
     st.write("To begin the process, please rate ten keywords to reflect your preferences.")
 
 else:
